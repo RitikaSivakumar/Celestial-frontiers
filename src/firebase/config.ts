305969e37635fb72f15d49a44445953dd77101a4
Prasheetha Karthikeyan <prasheethakarthikeyan@gmail.com>
2025-10-30 23:46:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4143885478-e2847",
+  "appId": "1:62113440619:web:2dc53d6084dd76df2f222e",
+  "apiKey": "AIzaSyBkiJpeQxaS9BLZ8XR2m2UcyQvk60TrvDA",
+  "authDomain": "studio-4143885478-e2847.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "62113440619"
+};
